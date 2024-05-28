b069479607ac94e7964406bfd15930a43da9351d
e18rayan <167943810+e18rayan@users.noreply.github.com>
2024-05-28 13:47:40 -0700

    Update test_protein_tool.py
    updating file

--- test_protein_tool.py
+++ test_protein_tool.py
@@ -1,2 +1,18 @@
 import pytest
-from Protein-Info-Tool import seq_to_prot, translate, prot_length, prot_MW, ex_coef
+from Protein-Info import translate, prot_length, prot_MW, ex_coef
+
+def test_translate():
+  assert translate("ATGCACTAA") == "MH*"
+  assert translate("TTATCTTGA") == "LS*"
+  assert translate("ATGTAA") == "M*"
+  with pytest.
+
+def test_prot_length():
+
+
+def test_prot_MW():
+
+
+def test_ex_coef():
+
+
